093f85f1e15194886ec6f218d37e2550bf955f97
bogdan.oprea <bogdan.oprea@avira.com>
2020-06-08 06:28:01 +0300

    Added fuckin header

--- ReaperTonMidiRoll/ReaperTon StepInsertOrModifyHeldNotesByGridLeft.lua
+++ ReaperTonMidiRoll/ReaperTon StepInsertOrModifyHeldNotesByGridLeft.lua
@@ -1,17 +1,16 @@
---[[
-@description ReaperTon Step - move Cursor Left By Grid Size And Alter Duration Of Held Notes
-@version 1.0
-@changelog
-    initial release
-@author obogz, thanks tenfour
-@provides 
-    core_midi_roll.lua
-    [main=midi_editor] .
-@about
-    # Fast midi step tool 
-@license MIT
-@donation https://paypal.me/obogz
-]]--
+-- @description ReaperTon Step - move Cursor Left By Grid Size And Alter Duration Of Held Notes
+-- @version 1.0
+-- @changelog
+--     initial release
+-- @author obogz, thanks tenfour
+-- @provides 
+--    core_midi_roll.lua
+--    [main=midi_editor] .
+-- @about
+--    # Fast midi step tool 
+-- @license MIT
+-- @donation https://paypal.me/obogz
+
 dofile(reaper.GetResourcePath().."/Scripts/obogz/core_midi_roll.lua")
 reaper.Undo_BeginBlock()
 insertOrModifyHeldNotesByGrid(-1)
