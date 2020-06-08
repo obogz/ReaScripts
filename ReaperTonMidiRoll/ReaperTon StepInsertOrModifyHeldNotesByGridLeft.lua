4cf8fbdf930b6739943cf04c28b36bfba0bb026b
bogdan.oprea <bogdan.oprea@avira.com>
2020-06-08 06:48:06 +0300

    small text issue

--- ReaperTonMidiRoll/ReaperTon StepInsertOrModifyHeldNotesByGridLeft.lua
+++ ReaperTonMidiRoll/ReaperTon StepInsertOrModifyHeldNotesByGridLeft.lua
@@ -1,20 +1,15 @@
---[[
-    Description: ReaperTon Step - move Cursor Left By Grid Size And Alter Duration Of Held Notes
-    Version: 1.1
-    Author: obogz, thanks tenfour
-    Donation: https://paypal.me/obogz
-    Changelog:
-        Fix: initial commit
-    Links:
-        Forum Thread https://forum.cockos.com/showthread.php?p=2141684
-        Lokasenna's Website http://forum.cockos.com/member.php?u=10417
-    About:
-        Fast midi step tool
-    Provides:
-        core_midi_roll.lua
-        [main=midi_editor] .
-]]--
-  
+-- @description ReaperTon - move Cursor Left By Grid Size And Alter Duration Of Held Notes
+-- @version 1.0
+-- @author obogz, thanks tenfour
+-- @about
+--   Fast midi step tool
+-- @changelog
+--   Initial
+-- @provides
+--   core_midi_roll.lua
+--   [main=midi_editor] .
+-- @donation https://paypal.me/obogz
+
 
 dofile(reaper.GetResourcePath().."/Scripts/obogz/core_midi_roll.lua")
 reaper.Undo_BeginBlock()
