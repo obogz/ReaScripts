c5cb8cd0175e6d6578aaf38643ebf22f273366ea
bogdan.oprea <bogdan.oprea@avira.com>
2020-06-08 06:31:09 +0300

    headers...

--- ReaperTonMidiRoll/ReaperTon StepInsertOrModifyHeldNotesByGridLeft.lua
+++ ReaperTonMidiRoll/ReaperTon StepInsertOrModifyHeldNotesByGridLeft.lua
@@ -1,15 +1,20 @@
--- @description ReaperTon Step - move Cursor Left By Grid Size And Alter Duration Of Held Notes
--- @version 1.0
--- @changelog
---     initial release
--- @author obogz, thanks tenfour
--- @provides 
---    core_midi_roll.lua
---    [main=midi_editor] .
--- @about
---    # Fast midi step tool 
--- @license MIT
--- @donation https://paypal.me/obogz
+--[[
+    Description: ReaperTon Step - move Cursor Right By Grid Size And Alter Duration Of Held Notes
+    Version: 1.1
+    Author: obogz, thanks tenfour
+    Donation: https://paypal.me/obogz
+    Changelog:
+        Fix: initial commit
+    Links:
+        Forum Thread https://forum.cockos.com/showthread.php?p=2141684
+        Lokasenna's Website http://forum.cockos.com/member.php?u=10417
+    About:
+        Fast midi step tool
+    Provides:
+        core_midi_roll.lua
+        [main=midi_editor] .
+]]--
+  
 
 dofile(reaper.GetResourcePath().."/Scripts/obogz/core_midi_roll.lua")
 reaper.Undo_BeginBlock()
--- ReaperTonMidiRoll/ReaperTonStepInsertOrModifyHeldNotesByGridRight.lua
+++ ReaperTonMidiRoll/ReaperTonStepInsertOrModifyHeldNotesByGridRight.lua
@@ -1,6 +1,6 @@
 --[[
-    Description: ReaperTon Step - move Cursor Left By Grid Size And Alter Duration Of Held Notes
-    Version: 1.0
+    Description: ReaperTon Step - move Cursor Right By Grid Size And Alter Duration Of Held Notes
+    Version: 1.1
     Author: obogz, thanks tenfour
     Donation: https://paypal.me/obogz
     Changelog:
@@ -10,9 +10,9 @@
         Lokasenna's Website http://forum.cockos.com/member.php?u=10417
     About:
         Fast midi step tool
-        [main=midi_editor] .
     Provides:
         core_midi_roll.lua
+        [main=midi_editor] .
 ]]--
   
 dofile(reaper.GetResourcePath().."/Scripts/obogz/core_midi_roll.lua")
